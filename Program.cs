1f212d24beb6d42c2fe3f17e13a8ba8f88a196f5
Moonyll <s.pouwel@laposte.net>
2018-12-19 10:22:18 +0100

    Bex5

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string[] week = new string[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" };
+        for (int i = 0; i < week.Length; i++) // week.Length > récupère l'indice du tableau
+        Console.WriteLine(week[i]);
+
+        }
+    }
+}
